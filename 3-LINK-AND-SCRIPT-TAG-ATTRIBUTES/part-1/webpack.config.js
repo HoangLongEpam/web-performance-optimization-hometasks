c5a4d496f3ba7c426b619f71ef8a3d1c5e87a38e
HoangLong256 <long_nguyen1@epam.com>
2024-05-02 14:37:41 +0700

    Module 3: Link and script tag attributes

--- 3-LINK-AND-SCRIPT-TAG-ATTRIBUTES/part-1/webpack.config.js
+++ 3-LINK-AND-SCRIPT-TAG-ATTRIBUTES/part-1/webpack.config.js
@@ -1,26 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            /**
-             * Make changes here
-            **/
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        })
+  entry: path.resolve(__dirname, "./index.js"),
+  output: {
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
