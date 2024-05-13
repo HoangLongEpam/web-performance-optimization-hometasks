681f320f151cb9150b6427e145417daef7e4539c
HoangLong256 <long_nguyen1@epam.com>
2024-05-13 23:21:33 +0700

    Module 5: CSS and Font optimization

--- 5-FONTS-AND-CSS/webpack.config.js
+++ 5-FONTS-AND-CSS/webpack.config.js
@@ -1,39 +1,46 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-/*
-* Make changes here
-*/
-const path = require('path');
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin(),
-        /*
-         * Make changes here
-        */
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, "dist"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      minify: true,
+      extract: true,
+      dimensions: [
+        { width: 320, height: 480 }, // iPhone 5
+        { width: 768, height: 1024 }, // iPad
+        { width: 1920, height: 1080 }, // Desktop
+      ],
+      penthouse: {
+        blockJSRequests: false,
+      },
+    }),
+  ],
 };
