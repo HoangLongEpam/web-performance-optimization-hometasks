cc24823e76a82cb7d38d99147219bca9bdb7abf3
HoangLong256 <long_nguyen1@epam.com>
2024-05-22 00:09:37 +0700

    Module 6: Code splitting

--- 6-CODE-SPLITTING/webpack.config.js
+++ 6-CODE-SPLITTING/webpack.config.js
@@ -1,13 +1,34 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    script1: './src/script1.js',
-    script2: './src/script2.js',
+    script1: "./src/script1.js",
+    script2: "./src/script2.js",
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: "[name].bundle.js",
   },
   optimization: {
     minimize: false,
-  }
+    splitChunks: {
+      chunks: "async",
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        lodash: {
+          test: /[\\/]node_modules[\\/]lodash[\\/]/,
+          name: "vendors",
+          enforce: true,
+          chunks: "all",
+        },
+      },
+    },
+  },
+  plugins: [new BundleAnalyzerPlugin()],
 };
